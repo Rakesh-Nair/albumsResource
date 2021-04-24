9fd942f89bc72e2d88e36301d85d3cd183e04f33
raknair <rakesh.nair@accelya.com>
2021-04-24 18:12:54 +0530

    Updated dependency for Webclient

--- src/main/java/com/appsdeveloperblog/ws/api/albums/AlbumsApplication.java
+++ src/main/java/com/appsdeveloperblog/ws/api/albums/AlbumsApplication.java
@@ -3,6 +3,9 @@ package com.appsdeveloperblog.ws.api.albums;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
@@ -13,7 +16,11 @@ public class AlbumsApplication {
 	}
 
 	@Bean
-	public WebClient webClient() {
-		return WebClient.builder().build();
+	public WebClient webClient(ClientRegistrationRepository clientRegistrationRepository,
+			OAuth2AuthorizedClientRepository oAuth2AuthorizedRepository) {
+		ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 = new ServletOAuth2AuthorizedClientExchangeFilterFunction(
+				clientRegistrationRepository, oAuth2AuthorizedRepository);
+		oauth2.setDefaultOAuth2AuthorizedClient(true);
+		return WebClient.builder().apply(oauth2.oauth2Configuration()).build();
 	}
 }
